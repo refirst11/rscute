bcb697305a430d16240d22eb5718f9cde12a9ee0
refirst11 <ocelot2828@gmail.com>
2025-10-01 20:17:01 +0900

    chore(bundle): removed

--- bundle/cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const { execute } = require('./execute.js');
-
-if (process.argv[2]) {
-  execute(process.argv[2]).catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
-}
