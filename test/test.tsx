b638e99adfe016fbcb6c5cf60aa78f1d04609982
refirst11 <ocelot2828@gmail.com>
2025-11-08 20:44:36 +0900

    test(test.tsx): update various import cases

--- test/test.tsx
+++ test/test.tsx
@@ -1,10 +1,12 @@
-import ESM_D from './ESM_D';
-import { ESM_N } from './ESM_N';
+import * as x from './ESM_D';
+import { ESM_N as AP } from './ESM_N';
 
 const CJS_D = require('./CJS_D');
 const { CJS_N } = require('./CJS_N');
-
-ESM_D();
-ESM_N();
+console.log(import.meta.url);
+console.log(x.bsumu);
+console.log(x.asumu);
+x.default();
+AP();
 CJS_D();
 CJS_N();
