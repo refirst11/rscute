6c7a7de89754d094abb83565ea5dea25236650c2
refirst11 <ocelot2828@gmail.com>
2025-11-14 19:09:10 +0900

    test(execute-code-api-test.js): function export test

--- test/execute-code-api-test.js
+++ test/execute-code-api-test.js
@@ -3,12 +3,16 @@ const { executeCode } = require('../dist/execute.js');
 const code = `
 export default { a: 1 };
 export const a = 2;
+export function collect() {
+ return 'function is collect';
+};
 `;
 
 async function abs() {
-  const { a, default: toml } = await executeCode(code);
-  console.log(toml.a);
+  const { default: df, a, collect } = await executeCode(code);
+  console.log(df.a);
   console.log(a);
+  console.log(collect());
 }
 
 abs();
