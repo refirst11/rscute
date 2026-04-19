4ca65338dad5bd7a0f9515c2146beb5d0d7ea6e9
refirst11 <ocelot2828@gmail.com>
2026-04-19 17:56:55 +0900

    chore: update register-api-test.js

--- test/register-api-test.js
+++ test/register-api-test.js
@@ -3,3 +3,5 @@ const { register } = require('../dist/register.js');
 register();
 
 require('./register-test-target.ts');
+const message = 'register-test-target.js has been executed.';
+console.log(message);
