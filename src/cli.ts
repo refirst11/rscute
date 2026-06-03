5209523de8e728a6ab2cd1012fbcacb58055aeb1
refirst11 <ocelot2828@gmail.com>
2026-06-03 14:19:49 +0900

    chore: update cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -7,7 +7,9 @@ const hookPath = join(__dirname, './index.js');
 const files = process.argv.slice(2).filter(arg => !arg.startsWith('-'));
 if (!files.length) throw new Error('No files specified.');
 
-const { error, status } = spawnSync(process.execPath, ['-r', hookPath, ...files], { stdio: 'inherit' });
+const args = ['-r', hookPath, ...process.argv.slice(2)];
+
+const { error, status } = spawnSync(process.execPath, args, { stdio: 'inherit' });
 
 if (error) throw error;
 process.exit(status ?? 1);
