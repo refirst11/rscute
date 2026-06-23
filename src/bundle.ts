7ea77167f764f808958289d0b8bdc05a530e5bec
refirst11 <ocelot2828@gmail.com>
2026-06-23 12:16:04 +0900

    chore: removed

--- src/bundle.ts
+++
@@ -1,7 +0,0 @@
-import { compiler } from './compiler';
-import { readFileCached } from './utils';
-
-export function bundle(filePath: string) {
-  const source = readFileCached(filePath);
-  return compiler(source, filePath);
-}
